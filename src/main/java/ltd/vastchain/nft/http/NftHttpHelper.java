0dc1afbf01d28c50915a1143d757cf35da5ed572
chenxiandiao <15068808239@163.com>
2022-01-11 14:05:20 +0800

    feat: 代码优化

--- src/main/java/ltd/vastchain/nft/http/NftHttpHelper.java
+++ src/main/java/ltd/vastchain/nft/http/NftHttpHelper.java
@@ -1,18 +1,9 @@
 package ltd.vastchain.nft.http;
 
-import com.google.gson.JsonObject;
-import ltd.vastchain.nft.http.model.HttpResponseModel;
 import ltd.vastchain.nft.http.model.Trait;
-import ltd.vastchain.nft.http.model.UserToken;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -20,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,96 +20,91 @@ import java.util.Map;
 public class NftHttpHelper {
 
     private static final String EMPTY_STR = "";
-//    private static NftHttpHelper instance = new NftHttpHelper();
     public static String SERVER = "http://10.144.1.94:8100";
-//    private String token = "";
-//
-//    public String getToken() {
-//        return token;
-//    }
-//
-//    public void setToken(String token) {
-//        this.token = token;
-//    }
 
-//    @Bean
-//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        return builder.build();
-//    }
 
-//    @Autowired
-//    private RestTemplate restTemplate;
-
-//    public static NftHttpHelper getInstance() {
-//        return instance;
-//    }
-
-//    public HttpHeaders getCommonHeaders(String token) {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.set("Cookie", "accessToken=" + token);
-//        return headers;
-//    }
-
-    public HttpHeaders getCommonHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Cookie", "accessToken=" + "pbfqglebcb");
-        return headers;
+    private Map createHeaderMap(String token) {
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put("Cookie", "accessToken=" + token);
+        return headerMap;
     }
 
+    /**
+     *
+     * @param appId
+     * @param appSecret
+     * @return
+     */
     public String getNftToken(String appId, String appSecret) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.GET_TOKEN;
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap();
         map.put("appId", appId);
         map.put("appSecret", appSecret);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, map, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        return postJson(restTemplate, url, null, map);
     }
 
-    public String createAccount(String mobile, String username) {
+    /**
+     *
+     * @param mobile
+     * @param username
+     * @param token
+     * @return
+     */
+    public String createAccount(String mobile, String username, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.CREATE_ACCOUNT;
-        HttpHeaders httpHeaders = getCommonHeaders();
-        Map<String, String> map = new HashMap();
+        Map<String, Object> map = new HashMap();
         map.put("mobile", mobile);
         map.put("username", username);
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
-    public String bindPublisher(String publisherAddress) {
+    /**
+     *
+     * @param publisherAddress
+     * @param token
+     * @return
+     */
+    public String bindPublisher(String publisherAddress, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.BIND_PUBLISH;
-        HttpHeaders httpHeaders = getCommonHeaders();
-        Map<String, String> map = new HashMap();
+        Map<String, Object> map = new HashMap();
         map.put("publisherAddress", publisherAddress);
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
-    public String bindUser(String mobile, String username) {
+    /**
+     *
+     * @param userAddress
+     * @param token
+     * @return
+     */
+    public String bindUser(String userAddress, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.CREATE_ACCOUNT;
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("UserAddress", mobile);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, map, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        Map<String, Object> map = new HashMap();
+        map.put("UserAddress", userAddress);
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
+    /**
+     *
+     * @param publisherAddress
+     * @param name
+     * @param symbol
+     * @param imageUrl
+     * @param description
+     * @param externalLink
+     * @param totalSupply
+     * @param token
+     * @return
+     */
     public String publishAsset(String publisherAddress, String name, String symbol,
                                String imageUrl, String description, String externalLink,
-                               int totalSupply) {
+                               int totalSupply, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.PUBLISH_ASSET;
-        HttpHeaders httpHeaders = getCommonHeaders();
         Map<String, Object> map = new HashMap();
         map.put("publisherAddress", publisherAddress);
         map.put("name", name);
@@ -128,19 +113,31 @@ public class NftHttpHelper {
         map.put("description", description);
         map.put("externalLink", externalLink);
         map.put("totalSupply", totalSupply);
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
+    /**
+     *
+     * @param publishAddress
+     * @param userAddress
+     * @param contractId
+     * @param author
+     * @param imagUrl
+     * @param name
+     * @param externalLink
+     * @param traits
+     * @param ownerMobile
+     * @param ownerName
+     * @param token
+     * @return
+     */
     public String createAssetNft(String publishAddress, String userAddress, String contractId, String author,
                                  String imagUrl, String name, String externalLink, List<Trait> traits,
-                                 String ownerMobile, String ownerName) {
+                                 String ownerMobile, String ownerName, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.NFT_ASSET_CREATE;
-        HttpHeaders httpHeaders = getCommonHeaders();
-        Map<String, String> map = new HashMap<String, String>();
+
+        Map<String, Object> map = new HashMap();
         map.put("publishAddress", userAddress);
         map.put("userAddress", userAddress);
         map.put("contractId", contractId);
@@ -148,124 +145,119 @@ public class NftHttpHelper {
         map.put("imagUrl", imagUrl);
         map.put("name", name);
         map.put("externalLink", externalLink);
-        // TODO: 2022/1/10
-//        map.put("traits", traits);
+        map.put("traits", traits);
         map.put("ownerMobile", ownerMobile);
         map.put("ownerName", ownerName);
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
-    public String getAssetInfo(String contractId, String tokenId) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpHeaders httpHeaders = getCommonHeaders();
-//        String url = SERVER + UrlConstants.NFT_ASSET_INFO + "/" + contractId + "/" + tokenId;
-//        System.out.println(url);
-//        HttpEntity<Map<String, String>> request = new HttpEntity(httpHeaders);
-//        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class, request);
-//        System.out.println(response.getBody());
-
+    /**
+     *
+     * @param contractId
+     * @param tokenId
+     * @param token
+     * @return
+     */
+    public String getAssetInfo(String contractId, String tokenId, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.NFT_ASSET_INFO + "/" + contractId + "/" + tokenId;
-        HttpHeaders headers = getCommonHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity(headers);
-        ResponseEntity<String> response = restTemplate.exchange(url,
-                HttpMethod.GET, entity, String.class);
-        System.out.println(response.getBody());
-
-        return response.getBody();
+        return get(restTemplate, url, createHeaderMap(token), null);
     }
 
-    public String getAssetList(int pageNum, int pageSize, String userAddress) {
+    /**
+     *
+     * @param pageNum
+     * @param pageSize
+     * @param userAddress
+     * @param token
+     * @return
+     */
+    public String getAssetList(int pageNum, int pageSize, String userAddress, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.NFT_ASSET_LIST;
-        HttpHeaders httpHeaders = getCommonHeaders();
         Map<String, Object> map = new HashMap();
         map.put("pageNum", pageNum);
         map.put("pageSize", pageSize);
         map.put("userAddress", userAddress);
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        System.out.println(restTemplate);
-        return response.getBody();
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
-    public String getAssetHistory(String contractId, String tokenId, int pageNum, int pageSize) {
+    /**
+     *
+     * @param contractId
+     * @param tokenId
+     * @param pageNum
+     * @param pageSize
+     * @param token
+     * @return
+     */
+    public String getAssetHistory(String contractId, String tokenId, int pageNum, int pageSize, String token) {
         RestTemplate restTemplate = new RestTemplate();
         String url = SERVER + UrlConstants.NFT_ASSET_HISTORY;
-        HttpHeaders httpHeaders = getCommonHeaders();
         Map<String, Object> map = new HashMap();
         map.put("contractId", contractId);
         map.put("tokenId", tokenId);
         map.put("pageNum", pageNum);
         map.put("pageSize", pageSize);
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+        return postJson(restTemplate, url, createHeaderMap(token), map);
     }
 
-    public String uploadFile(String file) {
-        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-        String url = SERVER + UrlConstants.NFT_ASSET_UPLOAD;
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
-        httpHeaders.set("Cookie", "accessToken=" + "pbfqglebcb");
-        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
-        map.add("file", new FileSystemResource(file));
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
-    }
-
-    public String uploadMultipartFile(MultipartFile file) throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-        String url = SERVER + UrlConstants.NFT_ASSET_UPLOAD;
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
-        httpHeaders.set("Cookie", "accessToken=" + "pbfqglebcb");
-        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+//    public String uploadFile(String file) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = SERVER + UrlConstants.NFT_ASSET_UPLOAD;
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+//        httpHeaders.set("Cookie", "accessToken=" + "pbfqglebcb");
+//        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
 //        map.add("file", new FileSystemResource(file));
-        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
-        ByteArrayResource byteArrayResource = new ByteArrayResource(file.getBytes()) {
-            @Override
-            public String getFilename() {
-                return file.getOriginalFilename();
-            }
-        };
-        map.add("file", byteArrayResource);
-        map.add("filename", file.getOriginalFilename());
-
-        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-        System.out.println(response.getBody());
-        return response.getBody();
-    }
-
-
-    public String getReceipt(String txId) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = SERVER + UrlConstants.NFC_ASSET_RECEIPT_QUERY;
-//        HttpHeaders httpHeaders = getCommonHeaders();
-        Map<String, Object> map = new HashMap();
-        map.put("txId", txId);
 //        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
 //        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 //        System.out.println(response.getBody());
 //        return response.getBody();
-        Map<String, String> headerMap = new HashMap();
-        headerMap.put("Cookie","accessToken=" + "pbfqglebcb");
+//    }
 
-        return postJson(restTemplate, url, headerMap, map );
+    /**
+     *
+     * @param file
+     * @param token
+     * @return
+     * @throws IOException
+     */
+    public String uploadMultipartFile(MultipartFile file, String token) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = SERVER + UrlConstants.NFT_ASSET_UPLOAD;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        httpHeaders.set("Cookie", "accessToken=" + token);
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        ByteArrayResource byteArrayResource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return System.currentTimeMillis() + "_" + file.getOriginalFilename() ;
+            }
+        };
+        map.add("file", byteArrayResource);
+        map.add("filename", file.getOriginalFilename());
+        HttpEntity<Map<String, String>> request = new HttpEntity(map, httpHeaders);
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+        System.out.println(response.getBody());
+        return response.getBody();
     }
 
+    /**
+     *
+     * @param txId
+     * @param token
+     * @return
+     */
+    public String getReceipt(String txId, String token) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = SERVER + UrlConstants.NFC_ASSET_RECEIPT_QUERY;
+        Map<String, Object> map = new HashMap();
+        map.put("txId", txId);
+        return postJson(restTemplate, url, createHeaderMap(token), map );
+    }
 
     /**
      * get请求
@@ -299,16 +291,16 @@ public class NftHttpHelper {
      * @param restTemplate
      * @param url
      * @param headerMap
-     * @param paramObjectStr
+     * @param map
      * @return
      */
-    public static String postJson(RestTemplate restTemplate, String url, Map<String,String> headerMap, Map<String,Object> paramObjectStr){
+    public static String postJson(RestTemplate restTemplate, String url, Map<String,String> headerMap, Map<String,Object> map){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         if(!CollectionUtils.isEmpty(headerMap)){
             headerMap.forEach((k,v)-> headers.set(k,v));
         }
-        String resultStr = restTemplate.postForObject(url,new HttpEntity<>(paramObjectStr,headers),String.class);
+        String resultStr = restTemplate.postForObject(url,new HttpEntity<>(map,headers),String.class);
         return resultStr;
     }
 
--- src/main/java/ltd/vastchain/nft/http/controller/HomeController.java
+++ src/main/java/ltd/vastchain/nft/http/controller/HomeController.java
@@ -3,7 +3,6 @@ package ltd.vastchain.nft.http.controller;
 
 import ltd.vastchain.nft.http.NftHttpHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,85 +16,75 @@ public class HomeController {
 
     static final String SERVER = "http://10.144.1.94:8100";
     static final String PUBLISH_ADDRESS = "a453659f8d2288c63dd2fe250f5ac8ab35750b42";
+    static final String token = "dxgqmyojqm";
 
     @Autowired
     NftHttpHelper helper;
 
     @RequestMapping(value = "getToken")
     public String getToken() {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
         System.out.println(helper);
         return helper.getNftToken("yl_test", "yl_test");
     }
 
     @RequestMapping(value = "createCount")
     public String createCount() {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
         System.out.println(helper);
-        return helper.createAccount("15068808239", "cxd");
+        return helper.createAccount("15068808239", "cxd",token);
     }
 
     @RequestMapping(value = "bindPublisher")
     public String bindPublisher() {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
-        return helper.bindPublisher(PUBLISH_ADDRESS);
+        return helper.bindPublisher(PUBLISH_ADDRESS,token);
     }
 
     @RequestMapping(value = "publishAsset")
     public String publishAsset() {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
         return helper.publishAsset(PUBLISH_ADDRESS,
                 "布尔熊",
                 "dgc",
                 ": https://gss0.bdstatic.com/5bd1bjqh_Q23odCf/static/indexatom/personalcenter/assets/img/default_icon_02f13d8.png",
-                "独一无二的艺术数字典藏", "", 1000);
+                "独一无二的艺术数字典藏", "", 1000,token);
     }
 
     @RequestMapping(value = "createAsset")
     public String createAsset(@RequestParam("contractId") String contractId) {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
         return helper.createAssetNft(PUBLISH_ADDRESS, PUBLISH_ADDRESS, contractId,
                 "cxd", "", "布尔熊1号", "",
-                null, "15068808239", "cxd");
+                null, "15068808239", "cxd",token);
     }
 
     @RequestMapping(value = "getAssetInfo/{contractId}/{tokenId}")
     public String getAssetInfo(@PathVariable("contractId") String contractId, @PathVariable("tokenId") String tokenId) {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
-        return helper.getAssetInfo(contractId, tokenId);
+        return helper.getAssetInfo(contractId, tokenId,token);
     }
 
 
     @RequestMapping(value = "getAssetList")
     public String getAssetList() {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
         System.out.println(helper);
-        return helper.getAssetList(1, 10, PUBLISH_ADDRESS);
+        return helper.getAssetList(1, 10, PUBLISH_ADDRESS,token);
     }
 
     @RequestMapping(value = "getAssetHistory")
     public String getAssetHistory(@RequestParam("contractId") String contractId, @RequestParam("tokenId") String tokenId) {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
         System.out.println(helper);
-        return helper.getAssetHistory(contractId, tokenId, 1, 10);
+        return helper.getAssetHistory(contractId, tokenId, 1, 10,token);
     }
+
+//    @RequestMapping(value = "uploadFile")
+//    public String uploadFile() {
+//        return helper.uploadFile("/Users/cxd/Desktop/WechatIMG1.jpeg");
+//    }
 
     @RequestMapping(value = "uploadFile")
-    public String uploadFile() {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
-        return helper.uploadFile("/Users/cxd/Desktop/WechatIMG1.jpeg");
-    }
-
-    @RequestMapping(value = "uploadFileTest")
     public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
-        return helper.uploadMultipartFile(file);
+        return helper.uploadMultipartFile(file, token);
     }
 
     @RequestMapping(value = "getReceipt")
     public String getReceipt(@RequestParam("txId") String txId) {
-//        NftHttpHelper helper = NftHttpHelper.getInstance();
-        return helper.getReceipt(txId);
+        return helper.getReceipt(txId, token);
     }
 
 }
